fb9f58febc32d78d0307fe087a813cb517bd996a
Istvan Milak <istvan.milak@silabs.com>
2025-09-17 15:32:51 +0200

    WISUN_SDK-1677: add defs header

---
+++ inc/ws_br_agent_defs.h
@@ -0,0 +1,37 @@
+#ifndef WS_BR_AGENT_DEFS_H
+#define WS_BR_AGENT_DEFS_H
+
+#include <stdint.h>
+#include <stddef.h>
+#include <stdbool.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/// Application name
+#define WS_BR_AGENT_APP_NAME "Wi-SUN SoC Border Router Agent Service"
+
+/// Version of the Wi-SUN SoC Border Router Agent
+#ifndef WS_BR_AGENT_VERSION
+#define WS_BR_AGENT_VERSION "0.1"
+#endif
+
+/// Copyright string
+#define WS_BR_AGENT_APP_COPYRIGHT_STR "Copyright (c) 2025 Silicon Labs, Inc."
+
+/// Return codes
+#define WS_BR_AGENT_RET_OK 0
+#define WS_BR_AGENT_RET_ERR -1
+
+/// Port number for the Wi-SUN SoC Border Router Agent service
+#define WS_BR_AGENT_SERVICE_PORT 4567U
+
+/// Port number of the Wi-SUN SoC Border Router Agent SoC
+#define WS_BR_AGENT_SOC_PORT 5678U
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // WS_BR_AGENT_DEFS_H
